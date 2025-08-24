394eb256f20774160b686ba77cca97f92430ec1c
Phantom <ramonadedotun550@gmail.com>
2025-08-24 02:40:04 +0100

    Creating an admin feature to list all users

--- Back-End-Capstone/Store/users/admin.py
+++ Back-End-Capstone/Store/users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import UserProfile
 
 # Register your models here.
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "username", "email")
+    search_fields = ("username", "email")
+    list_filter = ("user__is_active",)
--- Back-End-Capstone/Store/users/models.py
+++ Back-End-Capstone/Store/users/models.py
@@ -5,7 +5,6 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
-    user_id = models.CharField(max_length=100, unique=True)
     username = models.CharField(max_length=100)
     email = models.EmailField(max_length=254, unique=True)
     password = models.CharField(max_length=128)
