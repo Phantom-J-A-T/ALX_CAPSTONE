477fda5c5c5f7bcaaaaf0f49337e1b12eb936253
Phantom <ramonadedotun550@gmail.com>
2025-08-31 14:32:11 +0100

    Updating the Add function in the API

--- Front-End-Capstone/store/src/utils/cart.js
+++ Front-End-Capstone/store/src/utils/cart.js
@@ -1,18 +1,10 @@
 import axios from "axios";
 
 export const getCart = () => axios.get("/api/cart/");
-
-// Add to cart (uses POST /items/)
 export const addToCartAPI = (productId, quantity) =>
   axios.post("/api/cart/items/", { product: productId, quantity });
-
-// Update cart item (PATCH /items/:id/)
 export const updateCartItem = (itemId, quantity) =>
   axios.patch(`/api/cart/items/${itemId}/`, { quantity });
-
-// Remove cart item (DELETE /items/:id/)
 export const removeCartItem = (itemId) =>
   axios.delete(`/api/cart/items/${itemId}/`);
-
-// Clear cart (DELETE /clear/)
 export const clearCartAPI = () => axios.delete("/api/cart/clear/");
