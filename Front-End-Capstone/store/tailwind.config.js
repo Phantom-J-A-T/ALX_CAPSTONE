eaa720c36fbd311f3b1e601da6070b17b31187f3
Phantom-J-A-T <ramonadedotun550@gmail.com>
2025-12-28 00:37:51 +0100

    Upgrading the Tailwind.config.js to match the new colour schemes

--- Front-End-Capstone/store/tailwind.config.js
+++ Front-End-Capstone/store/tailwind.config.js
@@ -1,10 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",  // 👈 add this
-  ],
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'royal-blue': '#0B4A8C',   // Taken from your logo's shield
+        'royal-gold': '#C5A059',   // Taken from the crowns/trim
+        'royal-light': '#FAF9F6',  // Off-white for page backgrounds
+      },
+      fontFamily: {
+        'serif': ['"Playfair Display"', 'serif'], // For titles
+        'sans': ['Inter', 'sans-serif'],           // For UI/Body
+      }
+    },
   },
   plugins: [],
-};
+}
